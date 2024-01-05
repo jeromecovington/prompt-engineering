10f6d030dfff486c0d012d31e12ba72d53b49c38
Jerome Covington <jeromecovington@gmail.com>
2024-01-05 15:22:08 -0500

    Add fourth tactic

---
+++ examples/tactic-4.js
@@ -0,0 +1,24 @@
+// Tactic 4: "Few-shot" prompting
+
+const { getCompletion } = require('../lib/utils')
+
+const main = async () => {
+  const prompt = `
+Your task is to answer in a consistent style.
+
+<child>: Teach me about patience.
+
+<grandparent>: The river that carves the deepest
+valley flows from a modest spring; the
+grandest symphony originates from a single note;
+the most intricate tapestry begins with a solitary thread.
+
+<child>: Teach me about resilience.
+`
+
+  const completion = await getCompletion(prompt)
+
+  console.log(completion)
+}
+
+main()
